70805ede1738d504ac2d5072f7bcd09583549b66
light <852179641@qq.com>
2023-07-30 20:11:04 +0800

    Initial commit

---
+++ src/main/java/com/study/multithreadingstudy/MultithreadingStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.multithreadingstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultithreadingStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/multithreadingstudy/MultithreadingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.multithreadingstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadingStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
